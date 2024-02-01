541a63c62879cf464eeb5100c7a9145fa52a102a
Daniel Balshyk <danya2zxc@gmail.com>
2024-02-01 18:23:54 +0200

    lesson_04 added

---
+++ lesson_04/mro.py
@@ -0,0 +1,26 @@
+import time
+from pathlib import Path
+
+
+user_input = input("Enter the string for search: ")
+
+file_path = Path("rockyou.txt")
+
+start_time = time.time()
+
+try:
+    with file_path.open(
+        mode="r", encoding="utf-8", errors="ignore"
+    ) as new_file:
+        found_lines = 0
+        for index, line in enumerate(new_file, 1):
+            if user_input == line:
+                found_lines += 1
+except FileNotFoundError:
+    print(f"File '{file_path}' not found.")
+else:
+    end_time = time.time()
+    print(
+        f"While going through the entire file '{'rockyou.txt'}', lines were found '{found_lines}' mentioning your request '{user_input}'"
+    )
+    print(f"time spent on the operation: {end_time - start_time} seconds")
