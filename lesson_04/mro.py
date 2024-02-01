4a1beab6b8de114ed5cfeb7a02ac963091b25f08
Daniel Balshyk <danya2zxc@gmail.com>
2024-02-01 18:34:02 +0200

    some changes

--- lesson_04/mro.py
+++ lesson_04/mro.py
@@ -14,13 +14,15 @@ try:
     ) as new_file:
         found_lines = 0
         for index, line in enumerate(new_file, 1):
-            if user_input == line:
+            if user_input in line:
                 found_lines += 1
 except FileNotFoundError:
     print(f"File '{file_path}' not found.")
 else:
     end_time = time.time()
     print(
-        f"While going through the entire file '{'rockyou.txt'}', lines were found '{found_lines}' mentioning your request '{user_input}'"
+        f"While going through the entire file '{'rockyou.txt'}', "
+        f"lines were found {found_lines} mentioning your request "
+        f"'{user_input}'"
     )
     print(f"time spent on the operation: {end_time - start_time} seconds")
