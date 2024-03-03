b19841d7a3d23d19f219170ca9c546ff2d996f16
Daniel Balshyk <danya2zxc@gmail.com>
2024-03-03 15:45:19 +0200

    lesson_12 was updated

--- lesson_12/Multithreading_multiprocessing.py
+++ lesson_12/Multithreading_multiprocessing.py
@@ -12,9 +12,12 @@ key = Fernet.generate_key()
 fernet = Fernet(key)
 
 
+media_path_folder: Path = Path(__file__).resolve().parent / "media"
+if not media_path_folder.exists():
+    media_path_folder.mkdir()
 og_file_path: Path = Path("rockyou.txt")
-enc_file_path = Path(__file__).resolve().parent / "encrypted_rockyou.txt"
-photo_path: Path = Path(__file__).resolve().parent / "image.jpg"
+enc_file_path = media_path_folder / "encrypted_rockyou.txt"
+photo_path: Path = media_path_folder / "image.jpg"
 photo_url: str = "https://picsum.photos/1000/1000"
 
 
@@ -41,62 +44,47 @@ def encrypt_file(original_path: Path, encrypted_path: Path) -> None:
 
 
 # I/O-bound task (downloading image from URL)
-def download_image(image_url: str) -> None:
+def download_image(image_url: str, target_path: Path) -> None:
     print(
         f"Downloading image from {image_url} "
         f"in thread {threading.current_thread().name}"
     )
     response = requests.get(image_url)
-    with open(photo_path, "wb") as f:
+    with open(target_path, "wb") as f:
         f.write(response.content)
-
-
-def processes():
-    processes: list[Process | Thread] = [
-        Process(target=encrypt_file, args=(og_file_path, enc_file_path)),
-        Thread(target=download_image, args=(photo_url,)),
-    ]
-    for process in processes:
-        process.start()
-    for process in processes:
-        process.join()
 
 
 def main():
     try:
-        encryption_counter = time.perf_counter()
+        encryption_start_time = time.perf_counter()
 
-        # encrypt_process = Process(
-        #     target=encrypt_file, args=(og_file_path, enc_file_path))
-        # encrypt_process.start()
-        # encrypt_process.join()
-        encrypt_file(og_file_path, enc_file_path)
+        encrypt_process = Process(
+            target=encrypt_file, args=(og_file_path, enc_file_path)
+        )
+        encrypt_process.start()
 
-        download_counter = time.perf_counter()
+        download_start_time = time.perf_counter()
 
-        # download_thread = Thread(target=download_image, args=(photo_url,))
-        # download_thread.start()
-        # download_thread.join()
+        download_thread = Thread(
+            target=download_image, args=(photo_url, photo_path)
+        )
+        download_thread.start()
 
-        download_image(photo_url)
-        total = time.perf_counter() - encryption_counter
+        encrypt_process.join()
+        encryption_time = time.perf_counter() - encryption_start_time
+
+        download_thread.join()
+        download_time = time.perf_counter() - download_start_time
+
+        total = encryption_time + download_time
 
         print(
-            f"Time taken for encryption task: {encryption_counter},"
-            f"I/O-bound task: {download_counter}, Total: {total} seconds"
+            f"Time taken for encryption task: {encryption_time},"
+            f"I/O-bound task: {download_time}, Total: {total} seconds"
         )
     except Exception as e:
         print(f"Error occurred: {e}")
 
 
 if __name__ == "__main__":
-    total_start = time.perf_counter()
-
-    start = time.perf_counter()
-    processes()
-    end = time.perf_counter()
-    print(f"total process time was {end- start}\n")
-
     main()
-
-    print(f"\nTotal time was {time.perf_counter() - total_start}")
